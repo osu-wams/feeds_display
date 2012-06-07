7e4b70a4734c286998b361a892d4970ffa3eddea
Paul Lieberman <paul.lieberman@oregonstate.edu>
2012-06-07 14:59:34 -0700

    Fixes javascript

--- live_feeds.js
+++ live_feeds.js
@@ -3,19 +3,23 @@
 // Prefill the Title and URL
 // with the defaults for this feed type
 
-Drupal.behaviors.live_feeds = function(context) {
-  $('#edit-feed-type').change(function() {
-   	var feed_type  = $('#edit-feed-type').val();
-    if (feed_type == 'select') {
-      $('#edit-title').val('');
-      $('#edit-feed-url').val('');
+(function ($) {
+  Drupal.behaviors.live_feeds = {
+    attach: function(context, settings) {
+        $('#edit-feed-type').change(function() {
+        var feed_type  = $('#edit-feed-type').val();
+        if (feed_type == 'select') {
+          $('#edit-title').val('');
+          $('#edit-feed-url').val('');
+        }
+        else {
+          var feed_title = Drupal.settings.feeds_display[feed_type].name;
+          var feed_url   = Drupal.settings.feeds_display[feed_type].url;
+          $('#edit-title').val(feed_title);
+          $('#edit-feed-url').val(feed_url);
+        }
+      });
     }
-    else {
-      var feed_title = Drupal.settings.live_feeds[feed_type].name;
-      var feed_url   = Drupal.settings.live_feeds[feed_type].url;
-    
-      $('#edit-title').val(feed_title);
-      $('#edit-feed-url').val(feed_url);
-		}
-  });
-}
+  };
+})(jQuery);
+
