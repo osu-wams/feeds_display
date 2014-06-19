edcbc0a2daf97b84b97152b453c7561b9ef2a064
Richard Middaugh <ricky.middaugh@oregonstate.edu>
2014-06-19 13:42:07 -0700

    Set feed title, url default on feed type selection
    re #27652

--- live_feeds.js
+++ live_feeds.js
@@ -5,17 +5,17 @@
 (function ($) {
   Drupal.behaviors.live_feeds = {
     attach: function(context, settings) {
-        $('#edit-feed-type').change(function() {
-          var feed_type  = $('#edit-feed-type').val();
+        $('#edit-feed-type-und').change(function() {
+          var feed_type  = $('#edit-feed-type-und').val();
           if (feed_type == 'select') {
-            $('#edit-title').val('');
-            $('#edit-feed-url').val('');
+            $('#edit-feed-title-und-0-value').val('');
+            $('#edit-url-und-0-value').val('');
           }
           else {
             var feed_title = Drupal.settings.live_feeds[feed_type].name;
             var feed_url   = Drupal.settings.live_feeds[feed_type].url;
-            $('#edit-title').val(feed_title);
-            $('#edit-feed-url').val(feed_url);
+            $('#edit-feed-title-und-0-value').val(feed_title);
+            $('#edit-url-und-0-value').val(feed_url);
           }
       });
    }
