78edd63198eb569742243e294c2c1e0f2784a8fe
Matthew Brabham <matthew.brabham@oregonstate.edu>
2016-11-16 12:19:36 -0800

    Trying to create a service to get feeds

--- src/GetFeed.php
+++ src/GetFeed.php
@@ -2,18 +2,38 @@
 
 namespace Drupal\live_feeds;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7;
+
 /**
  * Class GetFeed.
  *
  * @package Drupal\live_feeds
  */
 class GetFeed {
+  protected $httpClient;
 
   /**
    * Constructor.
    */
-  public function __construct() {
+  public function __construct(Client $httpClient) {
+    $this->httpClient = $httpClient;
+  }
 
+  public function getFeed($feed_url) {
+    // Try to request the feed.
+    try {
+      $request = $this->httpClient->request('GET', $feed_url);
+      $response = $request->getBody();
+      $file_contents = preg_replace('/[^[:print:]\r\n]/', '', $response);
+      $xml = simplexml_load_string($file_contents);
+      return $xml;
+    } catch (RequestException $e) {
+      // Log the failed request to watchdog.
+      watchdog_exception('live_feeds', $e);
+    }
+    return FALSE;
   }
 
 }
