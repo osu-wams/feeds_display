8b0d1645a9db99d9d8c431de85609dfa2aca6ea9
Matthew Brabham <matthew.brabham@oregonstate.edu>
2024-12-20 13:47:39 -0800

    Add custom exception for feed parsing errors
    Introduce `FeedsDisplayParserException` to replace generic exceptions when feed parsing fails. Update logging to handle and specify parser-related errors, improving error clarity and debugging.

---
+++ src/Exception/FeedsDisplayParserException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\live_feeds\Exception;
+
+/**
+ * Exception for when there is a problem with the parser of a feed.
+ */
+class FeedsDisplayParserException extends \Exception {
+
+}
